d2137c231d29e9b616ff78b6a0c767c0b25baeec
shakhbozm <shfazliddinov1940@gmail.com>
2022-03-28 11:34:13 +0500

    reso

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -120,7 +120,7 @@ const Categories = () => {
   return (
     <Container>
       <Info>
-          <Title>plo Categories</Title>
+          <Title>plod Categories</Title>
           <Desc><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptates!</span> <span style={{height:'3px', width:"20%",background:"#ffc107"}} ></span></Desc>
       </Info>
 
